58ed62086bf9571f23e5b9a3b8c217da85aa27ec
Sithija Shehara <nsithijashehara@gmail.com>
2024-03-05 22:13:06 +0530

    removed gpt dev

--- gpt dev/client/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import send from "./send.svg";
-import logo from "./logo.png";
-import user from "./user.png";
-import bot from "./bot.png";
-import loadingIcon from "./loader.svg";
-
-export { send, logo, user, bot, loadingIcon };
